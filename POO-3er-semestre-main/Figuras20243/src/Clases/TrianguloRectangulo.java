e19f26ac43113ae13180c237e15937a55dd43f18
BLstudentman99 <143562661+BLstudentman99@users.noreply.github.com>
2024-08-29 12:58:54 -0600

    Add files via upload

---
+++ POO-3er-semestre-main/Figuras20243/src/Clases/TrianguloRectangulo.java
@@ -0,0 +1,21 @@
+package Clases;
+
+public class TrianguloRectangulo {
+    private int base;
+    private int altura;
+    
+    public int getBase(){
+        return this.base;
+    }
+    public void setBase(int base){
+        this.base = base;
+    }
+    public int getAltura(){
+        return this.altura;   
+    }
+    public void setAltura(int altura){
+        this.altura = altura;
+
+    }
+}
+
