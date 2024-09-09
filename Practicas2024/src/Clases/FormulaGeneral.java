1479027f4f85d58cbcc3133a5da0c347e7ffb410
BLstudentman99 <143562661+BLstudentman99@users.noreply.github.com>
2024-09-09 12:50:57 -0600

    Add files via upload

---
+++ Practicas2024/src/Clases/FormulaGeneral.java
@@ -0,0 +1,17 @@
+package Clases;
+
+public class FormulaGeneral {
+    private int a;
+    private int b;
+    private int c;
+    
+    public FormulaGeneral(int a, int b, int c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    
+    }
+    public int geta(){
+        return a;
+}
+}
